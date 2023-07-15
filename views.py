a018bf7788a8ba98ef4aaa31e960d36ec281e806
Alex <114286426+AlexBaranovIT@users.noreply.github.com>
2023-07-15 19:11:25 +0300

    To-Do list project

--- views.py
+++ views.py
@@ -10,7 +10,7 @@ class NewTaskForm(forms.Form):
 #Create a function that makes a new user session
 # Create your views here.
 def index(request):
-    if "tasks" not in request.session:
+    if "tasks" not in request.session: 
         request.session["tasks"] = []
     return render(request, "tasks/index.html", {
         "tasks": request.session["tasks"]
